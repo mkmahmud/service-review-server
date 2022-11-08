62d03191b4edc476ae399991cdcfd7ae65101598
mkmahmud <mahmudulhasanmk434@gmail.com>
2022-11-08 14:02:06 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -4,12 +4,33 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+// Data
+const servicesData = require('./services-data.json')
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello ')
+})
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.User_Name}:${process.env.User_Password}@cluster0.cjesyyc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/services', (req, res) => {
+    res.send(servicesData)
 })
 
 
