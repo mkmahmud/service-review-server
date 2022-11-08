d60045c3f3429903c5213fb62acd77bf7bfe5a1d
mkmahmud <mahmudulhasanmk434@gmail.com>
2022-11-08 14:15:09 +0600

    load data from mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 // Data
-const servicesData = require('./services-data.json')
+// const servicesData = require('./services-data.json');
+// const { async } = require('@firebase/util');
 
 // Middleware
 app.use(cors());
@@ -17,21 +21,36 @@ app.get('/', (req, res) => {
 
 
 
+// Mongo DB connection
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.User_Name}:${process.env.User_Password}@cluster0.cjesyyc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 
-app.get('/services', (req, res) => {
-    res.send(servicesData)
-})
+async function run () {
+
+    const servicesCollection = client.db('mk').collection('services')
+
+    try{
+
+        // Get services data from monogdb collection 
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const coursor = servicesCollection.find(query);
+            const servicesData = await coursor.toArray()
+            res.send(servicesData)
+        })
+        
+
+    }
+    catch{
+
+    }
+}
+
+
+run().catch(err => console.log(err))
+
 
 
 app.listen(port, () => {
